ded7f8f0cefba2c37cb899e73076bb9a284dd653
Matches121 <64685768+Matches121@users.noreply.github.com>
2020-08-04 12:24:43 +0200

    first comiit

---
+++ CALC.js
@@ -0,0 +1,49 @@
+var TotalCost = 0;
+
+$(document).ready(function() {
+    $("#submit").click(function() {
+        var CurrentCost = $("#CallCost").val();
+        $("#CallSave").html(`You would save R${CurrentCost*30/100}.`);
+        TotalCost = TotalCost + (CurrentCost * 30 / 100);
+        // $("#CallSave").show();
+    });
+});
+
+$(document).ready(function() {
+    $("#submit").click(function() {
+        var CurrentCost = $("#EquipCost").val();
+        $("#EquipSave").html(`You would save R${CurrentCost*15/100}.`);
+        TotalCost = TotalCost + (CurrentCost * 15 / 100);
+        // $("#CallSave").show();
+    });
+});
+
+$(document).ready(function() {
+    $("#submit").click(function() {
+        var CurrentCost = $("#LineCost").val();
+        if ($("ServiceProvider").val() === "Telkom") {
+            $("#LineSave").html(`You would save R${CurrentCost}.`);
+            TotalCost = TotalCost + (CurrentCost);
+        } else {
+            $("#LineSave").html(`You would save R${CurrentCost*30/100}.`);
+            TotalCost = TotalCost + (CurrentCost * 30 / 100);
+        };
+    });
+});
+
+$(document).ready(function() {
+    $("#submit").click(function() {
+        $("#TotalSave").html(`You would save a Estimated Total of R${TotalCost}.`);
+        // $("#CallSave").show();
+        TotalCost = 0;
+    });
+});
+
+$(document).ready(function() {
+    $("#submit").click(function() {
+        var CurrentCost = $("#InternetCost").val();
+        $("#InternetSave").html(`You would save R${CurrentCost*0/100}.`);
+        TotalCost = TotalCost + (CurrentCost * 0 / 100);
+        // $("#CallSave").show();
+    });
+});
